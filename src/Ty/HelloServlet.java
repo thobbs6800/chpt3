c7da8361af381aa9a66dcbe7190f83b75b44edff
Ty Hobbs <Ty@Tys-MacBook-Pro.local>
2017-02-13 17:31:04 -0600

    Comit

--- src/Ty/HelloServlet.java
+++ src/Ty/HelloServlet.java
@@ -1,6 +1,8 @@
 package Ty;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,7 +12,10 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HelloServlet
  */
-@WebServlet("/HelloServlet")
+@WebServlet(name ="HelloServlet",
+			urlPatterns ={"/greeting", "/salutation", "/wassup"},
+			loadOnStartup = 1
+		)
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -25,8 +30,32 @@ public class HelloServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().println("Hello World");
+	private static final String DEFAULT_USER = "Guest";
+	@Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String user = request.getParameter("user");
+		if(user == null)
+			user = HelloServlet.DEFAULT_USER;
+		
+		response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
+		
+		PrintWriter writer = response.getWriter();
+		writer.append("<!DOCTYPE html>\r\n");
+			writer.append("<html>\r\n");
+			writer.append("     <head>\r\n");
+			writer.append("     <title>Hello User App</title>\r\n");
+			writer.append("  </head>\r\n");
+			writer.append("    <body>\r\n");
+			writer.append( "     Hello, ").append(user).append("!<br/><br/>\r\n") ;
+			writer.append("     <form action =\"greeting\" method =\"POST\">\r\n");
+			writer.append("                    Enter your name: <br/>\r\n");
+			writer.append("                   <input type = \"text\" name=\"user\"/>\r\n");
+			writer.append("                   <input type =\"submit\" value =\"Submit\"/>\r\n");
+			writer.append("     </form>\r\n");
+			writer.append("     </body>\r\n");
+			writer.append("    </html>\r\n");
+		
 	}
 
 	/**
@@ -34,7 +63,7 @@ public class HelloServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		this.doGet(request, response);
 	}
 	@Override
 	public void init () throws ServletException
---
+++ src/Ty/MultiValueParameterServlet.java
@@ -0,0 +1,102 @@
+package Ty;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MultiValueParameterServlet
+ */
+@WebServlet(name ="multiValueParameterServlet",
+			urlPatterns ={"/checkboxes"}
+		)
+public class MultiValueParameterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MultiValueParameterServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/hmtl");
+		response.setCharacterEncoding("UTF-8");
+		
+		PrintWriter writer = response.getWriter();
+		
+		writer.append("<!DOCTYPE html>\r\n");
+		writer.append("<html>\r\n");
+		writer.append("     <head>\r\n");
+		writer.append("     <title>Hello User App</title>\r\n");
+		writer.append("  </head>\r\n");
+		writer.append("    <body>\r\n");
+		writer.append("   <form action =\"checkboxes\" method =\"POST\">\r\n ");
+		writer.append("Select the candy you like to eat: <br/>\r\n    ");
+		writer.append("<input type = \"checkbox\" name=\"fruit\" value =\"Banana\"/>\r\n    ");
+		writer.append(" Banana<br/>\r\n   ");
+		writer.append("<input type = \"checkbox\" name=\"fruit\" value =\"Apple\"/>\r\n    ");
+		writer.append(" Apple<br/>\r\n   ");
+		writer.append("<input type = \"checkbox\" name=\"fruit\" value =\"Orange\"/>\r\n    ");
+		writer.append(" Orange<br/>\r\n   ");
+		writer.append("<input type = \"checkbox\" name=\"fruit\" value =\"Strawberry\"/>\r\n    ");
+		writer.append(" Strawberry<br/>\r\n   ");
+		writer.append("<input type = \"checkbox\" name=\"fruit\" value =\"Pear\"/>\r\n    ");
+		writer.append(" Pear<br/>\r\n   ");
+		writer.append("<input type=\"submit\" value=\"Submit\"/>\r\n");
+		writer.append("</form>\r\n");
+		writer.append("</body>\r\n");
+		writer.append("</hmtl>\r\n");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String[] fruits = request.getParameterValues("fruit");
+		
+		response.setContentType("text/html");
+		response.setCharacterEncoding("UTF-8");
+		
+		PrintWriter writer = response.getWriter();
+		
+		writer.append("<!DOCTYPE html>\r\n");
+		writer.append("<html>\r\n");
+		writer.append("     <head>\r\n");
+		writer.append("     <title>Hello User App</title>\r\n");
+		writer.append("  </head>\r\n");
+		writer.append("    <body>\r\n");
+		writer.append("<h2>Your Selection</h2>\r\n");
+		
+		if(fruits == null)
+			writer.append("You did not select any fruits.\r\n");
+		else
+		{
+			writer.append("        <ul>\r\n");
+			for(String fruit : fruits)
+			{
+				writer.append("      <li>").append(fruit).append("</li>\r\n");
+			}
+			writer.append("           </ul>\r\n");
+			
+		}
+		writer.append("  </body>\r\n");
+		writer.append("</hmtl>\r\n");
+		
+	}
+
+}
